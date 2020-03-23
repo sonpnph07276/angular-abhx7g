dcee54dd334ee8853effe95142b5c7a9155a17d7
sonpnph07276 <sonpnph07276@fpt.edu.vn>
2020-03-23 14:14:38 +0700

    1 file modified, 1 added

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,19 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { AddProductComponent } from './add-product/add-product.component';
+import { AboutComponent } from './about/about.component';
+import { ProductListComponent } from './product-list/product-list.component';
+import { HomeComponent } from './home/home.component';
+
+
+const routes: Routes = [
+  { path: 'home', component: HomeComponent},
+  { path: 'add-product', component: AddProductComponent},
+  { path: 'about', component: AboutComponent}
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
--- src/app/services/product.service.ts
+++ src/app/services/product.service.ts
@@ -1,8 +1,37 @@
 import { Injectable } from '@angular/core';
+import { data } from '../MockData';
+import { Product } from '../Product';
 
 @Injectable()
 export class ProductService {
-
+  products = data;
   constructor() { }
 
+  getProducts(){
+      console.log('Hello');
+      return this.products;
+  }
+  getProduct(){
+
+  }
+  removeProduct(id){
+    return this.products = this.products.filter(product => product.id != id)
+  }
+  addProduct(product){
+
+    console.log(product);
+
+    let newObj = {id: 6,...product};
+    console.log(newObj);
+    
+    this.products.push(newObj);
+    console.log(this.products)
+
+  }
+  editProduct(){
+
+  }
+  addCartProduct(){
+
+  }
 }

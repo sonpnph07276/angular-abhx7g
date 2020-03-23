02359d0f04de10f01d05194632ac58d319f70e4c
sonpnph07276 <sonpnph07276@fpt.edu.vn>
2020-03-23 20:37:49 +0700

    2 files modified

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,12 +3,23 @@ import { Routes, RouterModule } from "@angular/router";
 import { AddProductComponent } from "./add-product/add-product.component";
 import { AboutComponent } from "./about/about.component";
 import { ProductListComponent } from "./product-list/product-list.component";
+import { NotFoundComponent } from "./not-found/not-found.component";
 import { HomeComponent } from "./home/home.component";
+import { ServicesComponent } from "./services/services.component";
+import { ContactComponent } from "./contact/contact.component";
+import { ProductDetailComponent } from "./product-detail/product-detail.component";
 
 const routes: Routes = [
+  { path: "", redirectTo: "home", pathMatch: "full" },
   { path: "home", component: HomeComponent },
-  { path: "add-product", component: AddProductComponent },
-  { path: "about", component: AboutComponent }
+  { path: "product", component: ProductListComponent },
+  { path: "product/add", component: AddProductComponent },
+  { path: "product/detail/:id", component: ProductDetailComponent },
+  { path: "about", component: AboutComponent },
+  { path: "services", component: ServicesComponent },
+  { path: "contact", component: ContactComponent },
+  { path: "404", component: NotFoundComponent },
+  { path: "**", redirectTo: "404", pathMatch: "full" }
 ];
 
 @NgModule({

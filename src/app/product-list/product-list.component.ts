42722967c4667f5aebd6a94e9c58c11da115a834
sonpnph07276 <sonpnph07276@fpt.edu.vn>
2020-03-25 20:04:49 +0700

    1 file modified

--- src/app/product-list/product-list.component.ts
+++ src/app/product-list/product-list.component.ts
@@ -1,15 +1,25 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { Product } from "../Product";
+import { ProductService } from "../product.service";
 
 @Component({
-  selector: 'app-product-list',
-  templateUrl: './product-list.component.html',
-  styleUrls: ['./product-list.component.css']
+  selector: "app-product-list",
+  templateUrl: "./product-list.component.html",
+  styleUrls: ["./product-list.component.css"]
 })
 export class ProductListComponent implements OnInit {
-
-  constructor() { }
+  selected: Product;
+  products: Product[];
+  constructor(private productService: ProductService) {}
 
   ngOnInit() {
+    console.log(this.products);
   }
-
+  showDetail(product){
+    this.selected= product;
+  }
+getProduct(){
+ this.products= this.productService.getProduct();
+ console.log(this.products);
+}
 }
